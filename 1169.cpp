7b17f17ca5168d37a58258441ecda7372faa15c6
accelsao <jay70550@yahoo.com.tw>
2017-10-25 15:38:32 +0800

    Update 1169.cpp

--- 1169.cpp
+++ 1169.cpp
@@ -17,21 +17,21 @@
 using namespace std;
 
 
-//dp[i] = (dp[j - 1] - cost[j] + dist[j]) + cost[i] + dist[i]
-int cost[10001], d[10001];
-int p[10001][3];//輸入點
-int dp[10001];
-int g[10001][10001];//實際距離
-int r[10001][10001];//障礙物
+const int M = 51;
+int cost[M], d[M];
+int p[M][3];//輸入點
+int dp[M];
+int g[M][M];//實際距離
+int r[M][M];//障礙物
 const int dw[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
 struct node {
 	int x, y, fs;
-	bool operator<(const node a)const {
+	bool operator<(const node &a)const {
 		return fs > a.fs;
 	}
 };
-int f[10001][10001];//f估價
-int v[10001][10001], n, m;//visit
+int f[M][M];//f估價
+int v[M][M], n, m;//visit
 int df(int j) {
 	return dp[j - 1] - cost[j] + d[j];
 }
@@ -39,7 +39,6 @@ int H(int sx, int sy, int tx, int ty) {
 	return abs(sx - tx) + abs(sy - ty);
 }
 int astar(int sx, int sy, int tx, int ty) {
-	int h = abs(sx - tx) + abs(sy - ty);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			v[i][j] = 0, g[i][j] = f[i][j] = 1e9;
@@ -56,11 +55,13 @@ int astar(int sx, int sy, int tx, int ty) {
 			int nx = cur.x + dw[k][0], ny = cur.y + dw[k][1];
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m || !r[nx][ny]||v[nx][ny])
 				continue;
-			q.push(node{ nx,ny,f[nx][ny] });
 			int ng = g[cur.x][cur.y] + 1;
-			if (ng >= g[nx][ny])continue;//this isnt a better path
+			if (ng >= g[nx][ny]) //this isnt a better path
+				continue;
+			
 			g[nx][ny] = ng;
 			f[nx][ny] = g[nx][ny] + H(nx, ny, tx, ty);
+			q.push(node{ nx,ny,f[nx][ny] });
 		}
 	}
 	return -1;
@@ -81,13 +82,12 @@ int main() {
 			cin >> a >> b; r[a][b] = 0;//0為障礙物
 		}
 		cin >> s;
-		p[0][0] = p[0][1] = p[0][2] = cost[0] = 0;
+		p[0][0] = x, p[0][1] = y;
+		p[0][2] = cost[0] = 0;
 		for (int i = 1; i <= s; i++) {
 			cin >> p[i][0] >> p[i][1] >> p[i][2];
-			//cost[i] = cost[i - 1] + abs(p[i][0] - p[i - 1][0]) + abs(p[i][1] - p[i - 1][1]);
-			cost[i] = cost[i - 1] + astar(p[i - 1][0], p[i - 1][1], p[i][0], p[i][1]);
-			p[i][2] += p[i - 1][2];
-			//d[i] = abs(p[i][0] - x) + abs(p[i][1] - y);
+			cost[i] = cost[i - 1] + astar(p[i - 1][0], p[i - 1][1], p[i][0], p[i][1]);//將點與點之間要走的距離(含S) 前綴和
+			p[i][2] += p[i - 1][2];//攜帶重量
 			d[i] = astar(x, y, p[i][0], p[i][1]);
 		}
 		deque<int>q;
@@ -99,8 +99,10 @@ int main() {
 				q.pop_front();
 			dp[i] = df(q.front())+ cost[i] + d[i];
 		}
-		printf("%d\n", dp[s]);
-		if (t)
-			puts("");
+		if (dp[s] <= 0)
+			puts("Mission Fail:(");
+		else
+			printf("%d\n", dp[s]);
 	}
 }
+

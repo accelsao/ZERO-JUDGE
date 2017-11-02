87fbe229f48ba961446faa210b8f34771f99215f
accelsao <jay70550@yahoo.com.tw>
2017-11-02 12:50:45 +0800

    Update c347.cpp

--- Code for I-O/c347.cpp
+++ Code for I-O/c347.cpp
@@ -37,19 +37,17 @@ int main() {
 	ios::sync_with_stdio(0), cin.tie(0);
 	FILE *f;
 	/*
-	freopen_s(&f, "c347_00.in.txt", "w", stdout);
-	int TN = 0, TP = 0, G = 0;
-	while (TN+TP<10) {
+	freopen_s(&f, "c347_03.in.txt", "w", stdout);
+	int TN = 0, TP = 0, G = 0, aa = 10;
+	while (TN+TP<aa) {
 		G++;
 		srand(time(0) + G);
-		int n = rand() % 91 + 10;
-		//cout << n << '\n';
+		int n = 1000;
 		char s = rand() % 2 ? 'S' : 'N';
-		//cout << s << '\n';
-
-		long long ans = 0, f = 0, z = 0, r[105], x[105], y[105], d[105];
+		const int sd = 2001;
+		long long ans = 0, f = 0, z = 0, r[sd], x[sd], y[sd], d[sd];
 		long long k = 0;
-		char c[105];
+		char c[sd];
 		int rc = 0;
 		for (int i = 0; i < n; i++) {
 			x[i] = rand() % M, y[i] = rand() % M;
@@ -114,13 +112,13 @@ int main() {
 	cout << "0\n";
 	*/
 	///*
-	freopen_s(&f, "c347_00.out.txt", "w", stdout);
+	freopen_s(&f, "c347_04.out", "w", stdout);
 	
 	int n;
 	char s;
 	while (cin >> n,n) {
 		cin >> s;
-		int ans = 0, x, y, r, f = 0, z = 0;
+		long long ans = 0, x, y, r, f = 0, z = 0;
 		char c;
 		for (int i = 0; i < n; i++) {
 			cin >> x >> y >> c;
@@ -144,3 +142,22 @@ int main() {
 	}
 	//*/
 }
+
+/*
+int mex[1024] = {}, SG[50][50];
+SG[0][0] = 0;
+for (int i = 0; i < 10; i++)
+for (int j = 0; j < 10; j++) {
+memset(mex, 0, sizeof(mex));
+for (int k = 1; k <= i; k++) {
+mex[SG[i - k][j]] = 1;//在後繼集合為1
+}
+for (int k = 1; k <= j; k++) {
+mex[SG[i][j - k]] = 1;//在後繼集合為1
+}
+int sg = 0;
+for (sg = 0; mex[sg]; sg++);//不在集合中的最小值 為sg值
+SG[i][j] = sg;
+printf("%d %d : %d\n", i,j, SG[i][j]);
+}
+*/
